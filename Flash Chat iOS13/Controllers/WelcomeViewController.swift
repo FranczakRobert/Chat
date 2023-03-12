e6bed8937c06171f372f0afaa6a74d5dd61c989c
RobertFranczak <94225378+RobertFranczak@users.noreply.github.com>
2023-03-12 19:42:43 +0100

    adding pods

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,23 +1,22 @@
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+//    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        var charIndex = 0
-        let title = "⚡️FoczekChat"
-        for letter in title {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { timer in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex+=1;
-        }
-       
+        titleLabel.text = "⚡️FoczekChat"
+//        var charIndex = 0
+//        let title = "⚡️FoczekChat"
+//        for letter in title {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex+=1;
+//        }
     }
-    
-
 }
